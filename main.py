12352d3d0a50e49c6e2817e65cb3c8c8566e0139
Andrew S <relics3@gmail.com>
2020-08-11 17:56:37 +0300

    changes

--- main.py
+++ main.py
@@ -3,8 +3,13 @@
 
 import cv2
 import numpy as np
+import tensorflow as tf
 from mtcnn import MTCNN
 from flask import Flask, request, jsonify
+
+physical_devices = tf.config.list_physical_devices('GPU')
+tf.config.experimental.set_memory_growth(physical_devices[0], True)
+
 
 detector = MTCNN()
 
@@ -20,8 +25,9 @@ def index():
 def send_result():
     nparr = np.fromstring(request.data, np.uint8)
     img_np = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+
     result = detector.detect_faces(img_np)
-    print(result)
+    # print(result)
     return jsonify(result)
 
 
